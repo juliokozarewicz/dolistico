9cbb9dca7438a06a54627109993d1f28d890a46f
Julio Kozarewicz <juliokozarewicz@gmail.com>
2025-12-09 12:18:46 -0300

    FEATURE( cache ) Refactor.

--- 1_deploy/config/cachemanager/init.sh
+++ 1_deploy/config/cachemanager/init.sh
@@ -3,19 +3,18 @@
 # ============================================================== ( set default init )
 set -e
 REDIS_CLI="redis-cli"
-echo "[CACHEMANAGER INIT] Testing connection"
-$REDIS_CLI PING || { echo "Redis is not accessible!"; exit 1; }
-
+echo "[CACHEMANAGER INIT] Testing connection..."
+$REDIS_CLI PING || { echo "Redis not accessible"; exit 1; }
 echo "[CACHEMANAGER INIT] Setting password for default user..."
 $REDIS_CLI ACL SETUSER default on ">$CACHEMANAGER_ADMIN_PASSWORD"
-
-REDIS_CLI_AUTH="$REDIS_CLI -a $CACHEMANAGER_ADMIN_PASSWORD"
+export REDISCLI_AUTH="$CACHEMANAGER_ADMIN_PASSWORD"
+redis-cli PING
 # =============================================================== ( set default end )
 
 # ================================================================= ( accounts init )
 if [ -n "$ACCOUNTS_CACHEMANAGER_USER" ] && [ -n "$ACCOUNTS_CACHEMANAGER_PASSWORD" ]; then
   echo "[CACHEMANAGER INIT] Creating Accounts user..."
-  $REDIS_CLI_AUTH ACL SETUSER "$ACCOUNTS_CACHEMANAGER_USER" on \
+  redis-cli ACL SETUSER "$ACCOUNTS_CACHEMANAGER_USER" on \
       ">$ACCOUNTS_CACHEMANAGER_PASSWORD" \
       resetkeys resetchannels \
       +publish +subscribe +psubscribe \
@@ -28,7 +27,7 @@ fi
 # ==================================================================== ( tasks init )
 if [ -n "$TASKS_CACHEMANAGER_USER" ] && [ -n "$TASKS_CACHEMANAGER_PASSWORD" ]; then
   echo "[CACHEMANAGER INIT] Creating Tasks user..."
-  $REDIS_CLI_AUTH ACL SETUSER "$TASKS_CACHEMANAGER_USER" on \
+  redis-cli ACL SETUSER "$TASKS_CACHEMANAGER_USER" on \
       ">$TASKS_CACHEMANAGER_PASSWORD" \
       resetkeys resetchannels \
       +publish +subscribe +psubscribe \
