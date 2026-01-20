71ff7813f951fc61d2779e66d479ced1bc297bd3
Julio Kozarewicz <juliokozarewicz@gmail.com>
2026-01-20 13:21:29 -0300

    FEATURE( streamingmanager ) Refactor.

--- 1_deploy/config/streamingmanager/init.sh
+++ 1_deploy/config/streamingmanager/init.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-echo "#########################################"
-echo "Starting Redpanda with SASL..."
-echo "#########################################"
-
 rpk redpanda start \
   --overprovisioned \
   --smp 1 \
@@ -14,47 +10,44 @@ rpk redpanda start \
   --kafka-addr PLAINTEXT://0.0.0.0:${STREAMINGMANAGER_PORT} \
   --advertise-kafka-addr PLAINTEXT://streamingmanager:${STREAMINGMANAGER_PORT} \
   --set redpanda.enable_sasl=true \
-  --set redpanda.superusers='["admin"]' &
+  --set redpanda.superusers='["${STREAMINGMANAGER_ADMIN_USER}"]' &
 
 REDPANDA_PID=$!
 
-echo "Waiting for Redpanda..."
 sleep 10
 
-echo "Creating admin user..."
-
-rpk acl user create admin \
-  --password admin123 \
+######################################################################## (admin init)
+rpk acl user create ${STREAMINGMANAGER_ADMIN_USER} \
+  --password ${STREAMINGMANAGER_ADMIN_PASSWORD} \
   --mechanism SCRAM-SHA-256 || true
-
-echo "Applying full ACL permissions to admin user..."
 
 # Cluster admin
 rpk acl create \
-  --allow-principal User:admin \
+  --allow-principal User:${STREAMINGMANAGER_ADMIN_USER} \
   --operation ALL \
   --cluster || true
 
 # All topics
 rpk acl create \
-  --allow-principal User:admin \
+  --allow-principal User:${STREAMINGMANAGER_ADMIN_USER} \
   --operation ALL \
   --topic '*' || true
 
 # All consumer groups
 rpk acl create \
-  --allow-principal User:admin \
+  --allow-principal User:${STREAMINGMANAGER_ADMIN_USER} \
   --operation ALL \
   --group '*' || true
 
 # All transactional IDs
 rpk acl create \
-  --allow-principal User:admin \
+  --allow-principal User:${STREAMINGMANAGER_ADMIN_USER} \
   --operation ALL \
   --transactional-id '*' || true
+######################################################################### (admin end)
 
 echo "#########################################"
-echo "Admin user ready with FULL access"
+echo "Redpanda and users successfully configured."
 echo "#########################################"
 
 wait $REDPANDA_PID
