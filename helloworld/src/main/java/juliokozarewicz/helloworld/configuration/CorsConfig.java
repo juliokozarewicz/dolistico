3ed5f362fb8129b89a40b18feddc05a415297818
Julio Kozarewicz <juliokozarewicz@gmail.com>
2026-02-26 12:14:02 -0300

    FEATURE( clean architecture ) Development.

--- helloworld/src/main/java/juliokozarewicz/helloworld/configuration/CorsConfig.java
+++ helloworld/src/main/java/juliokozarewicz/helloworld/configuration/CorsConfig.java
@@ -1,10 +1,9 @@
-package juliokozarewicz.accounts.configurations;
+package juliokozarewicz.helloworld.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 import java.util.ArrayList;
 import java.util.List;
 
