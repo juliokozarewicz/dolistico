d1c0a071649667701584201c132f3221a0a5cd3d
Julio Kozarewicz <juliokozarewicz@gmail.com>
2026-04-21 18:35:03 -0300

    FEATURE( accounts ) Create account development.

--- accounts/src/main/java/juliokozarewicz/accounts/application/usecase/AccountsCreateUseCase.java
+++ accounts/src/main/java/juliokozarewicz/accounts/application/usecase/AccountsCreateUseCase.java
@@ -6,7 +6,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class AccountsCreateUseCase {
 
-    public String execute(AccountsCreateCommand command) {
+    public String execute(
+
+        AccountsCreateCommand command
+
+    ) {
 
         String name = command.fullName() == null ? "" : command.fullName().trim();
         return "Account received for " + (name.isEmpty() ? "<unknown>" : name);
