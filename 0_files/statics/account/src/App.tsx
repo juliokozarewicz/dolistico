06d06818b40214c203af255db4273ad0f1ff03aa
Julio Kozarewicz <juliokozarewicz@gmail.com>
2026-06-22 21:57:05 -0300

    FEATURE( accounts ) Statics development.

---
+++ 0_files/statics/account/src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+function App() {
+  
+    const [count, setCount] = useState(0)
+
+    return (
+
+        <>
+            <main id="center">
+                <h1>App text!</h1>
+            </main>
+        </>
+
+    )
+
+}
+
+export default App
